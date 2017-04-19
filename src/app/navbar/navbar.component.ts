e20baf27b73592812e756097e013244d6997d32d
Stephen Wright <sjw948@gmail.com>
2017-04-19 22:39:13 +0100

    finished responsive navbar and html scaffolding for home component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,6 +9,22 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  showMenu: boolean = false;
+
+  menuToggle(resize?) {
+    if(resize && screen.width > 768) {
+      this.showMenu = true;
+      return {"visibility": "visible", "opacity": "1"}
+    } else if (resize && screen.width < 768) {
+      this.showMenu = false;
+      return {"visibility": "hidden", "opacity": "0"}
+    } else if (screen.width < 768 && this.showMenu == false) {
+      return {"visibility": "hidden", "opacity": "0"}
+    } else {
+      return {"visibility": "visible", "opacity": "1"}
+    }
+  }
+
   ngOnInit() {
   }
 
